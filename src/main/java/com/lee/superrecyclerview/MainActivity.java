82a597ad7c0bb60d07564ef49a92437f3bbb9f9d
lida <hebaulida@qq.com>
2016-07-19 23:29:49 +0800

    实现了下拉刷新，上拉自动加载

--- src/main/java/com/lee/superrecyclerview/MainActivity.java
+++ src/main/java/com/lee/superrecyclerview/MainActivity.java
@@ -2,6 +2,7 @@ package com.lee.superrecyclerview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -23,11 +24,32 @@ public class MainActivity extends Activity {
 	private GridLayoutManager mGridLayoutManager;
 	private StaggeredGridLayoutManager mStaggeredGridLayoutManager;
 	private Button add;
+	private SwipeRefreshLayout mSwipeRefreshLayout;
+	private int lastVisibleItem;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		add = (Button) findViewById(R.id.add);
+		mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.my_swiperefresh_view);
+
+		//下拉刷新数据
+		mSwipeRefreshLayout.setProgressBackgroundColorSchemeResource(R.color.google_green);
+		mSwipeRefreshLayout.setColorSchemeColors(R.color.google_blue,
+				R.color.google_red,
+				R.color.google_green,
+				R.color.google_black);
+		mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+			@Override
+			public void onRefresh() {
+				fetchingNewData();
+//				mSwipeRefreshLayout.setRefreshing(false);
+				mAdapter.notifyDataSetChanged();
+				Toast.makeText(getApplicationContext(), "shuaxin", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+
 		initData();
 
 		//创建并设置Adapter
@@ -36,7 +58,7 @@ public class MainActivity extends Activity {
 
 		mLinearLayoutManager = new LinearLayoutManager(this);
 		//垂直方向
-		mLinearLayoutManager.setOrientation(OrientationHelper.HORIZONTAL);
+		mLinearLayoutManager.setOrientation(OrientationHelper.VERTICAL);
 
 //		mGridLayoutManager = new GridLayoutManager(this, 4);
 
@@ -68,16 +90,43 @@ public class MainActivity extends Activity {
 		add.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				mAdapter.addItem("add item",1);
+				mAdapter.addItem("add item", 1);
 			}
 		});
-
+		//上拉刷新
+		mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+			@Override
+			public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+				super.onScrollStateChanged(recyclerView, newState);
+				if(newState == recyclerView.SCROLL_STATE_IDLE
+						&& lastVisibleItem + 1 == mAdapter.getItemCount()){
+					MoreData();
+					mAdapter.notifyDataSetChanged();
+				}
+			}
+			@Override
+			public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+				super.onScrolled(recyclerView, dx, dy);
+				lastVisibleItem =mLinearLayoutManager.findLastVisibleItemPosition();
+			}
+		});
 	}
 	protected void initData() {
 		mDatas = new ArrayList<String>();
-		for (int i = 'A'; i <= 'G'; i++)
+		for (int i = 'A'; i <= 'Z'; i++)
 		{
 			mDatas.add("" + (char) i);
+		}
+	}
+	private void fetchingNewData() {
+		for(int i = 0;i<5;i++) {
+			mDatas.add(i, "新数据" + i);
+		}
+		mSwipeRefreshLayout.setRefreshing(false);
+	}
+	private void MoreData() {
+		for(int i = 0;i<5;i++) {
+			mDatas.add( "新数据----" + i);
 		}
 	}
 }
