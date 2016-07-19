2cd8bb95450800cb27ad320efc7c291ec083937e
lida <hebaulida@qq.com>
2016-07-19 11:32:28 +0800

    del null

--- src/main/java/com/lee/superrecyclerview/MainActivity.java
+++ src/main/java/com/lee/superrecyclerview/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends Activity {
 		mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 		mRecyclerView.setAdapter(mAdapter);
 
-
 	}
 	protected void initData() {
 		mDatas = new ArrayList<String>();
