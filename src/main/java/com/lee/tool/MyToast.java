d8733f4760b8033fabc1268d5b5945c5593f2116
lida <hebaulida@qq.com>
2016-08-01 22:12:44 +0800

    调整格式

--- src/main/java/com/lee/tool/MyToast.java
+++ src/main/java/com/lee/tool/MyToast.java
@@ -9,12 +9,9 @@ import android.widget.Toast;
 public class MyToast {
 	private static Toast toast;
 
-	public static void showToast(Context context,
-								 String content) {
+	public static void showToast(Context context,String content) {
 		if (toast == null) {
-			toast = Toast.makeText(context,
-					content,
-					Toast.LENGTH_SHORT);
+			toast = Toast.makeText(context, content, Toast.LENGTH_SHORT);
 		} else {
 			toast.setText(content);
 		}
